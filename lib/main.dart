0cb25701fafce68efc11613ee9fba2295f47846b
Marc <mbruguerag@gmail.com>
2019-11-15 16:25:01 +0100

    canvi titol i scaffold nou

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Firebase Demo',
+      home: Scaffold(),
     );
   }
 }
-
