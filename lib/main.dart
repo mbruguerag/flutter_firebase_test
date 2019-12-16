d5afbf522420521dfad258a6191814a40a9dcd45
Thalia Diaz Juarez <thaliadiaz@10-192-87-168client.eduroam.upc.edu>
2019-12-16 16:16:40 +0100

    titol de monefy

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//BUENA
+void main() => runApp(Monefy());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Monefy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Demo',
-      home: Scaffold(),
+      home: ProfilePage(),
     );
   }
 }
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+                  child: Text(
+            'Monefy',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+          )
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
+
