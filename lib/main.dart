1ed481a95f0315250b3beecc26dc9bf780ccda7a
Thalia Diaz Juarez <thaliadiaz@MBP-de-Thalia.home>
2019-12-20 02:23:32 +0100

    usr sense funcions/ llista

--- lib/main.dart
+++ lib/main.dart
@@ -48,17 +48,18 @@ class _ProfilePageState extends State<ProfilePage> {
         crossAxisAlignment: CrossAxisAlignment.stretch, // centrem la imatge user
         children: <Widget>[
           Container(
-            height: 200,
+            height: 250,
             padding: EdgeInsets.only(bottom: 10),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               Container(
-                width: 70,
-                height: 70,
+                width: 100,
+                height: 100,
                 decoration: BoxDecoration(
                       image: DecorationImage(
-                        image: AssetImage('assets/usuario.jpg')
+                        image: AssetImage('assets/usuario.jpg'),
+                        fit: BoxFit.cover,
                   ),
                   shape: BoxShape.circle,
                   border: Border.all(
@@ -70,20 +71,23 @@ class _ProfilePageState extends State<ProfilePage> {
               Text('nom user',
                   style: TextStyle(
                     color: Colors.black,
+                    fontSize: 18,
                   ),
                   ),
-
+              
               Container(
-                color: Colors.white,
+              
                 padding: EdgeInsets.symmetric(vertical:10),
+                color: Colors.white10,
+                
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     Column(
                       children: <Widget>[
-                        Text('diners €',
+                        Text('300 €',
                           style: TextStyle(
-                            fontSize: 20,
+                            fontSize: 40,
                             fontWeight: FontWeight.bold,                           
                           ),
                         ),
@@ -91,11 +95,64 @@ class _ProfilePageState extends State<ProfilePage> {
                     )
                   ],
                 ),
-
-              )
+              ),
+              
             ],
           ),
-          )
+          ),
+          divider,
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                      Text('Operaciones')
+                    ],
+                  ),
+
+                Column(
+                  children: <Widget>[
+                      Text('Pendientes')
+                    ],
+                  ),
+              ],
+            ),
+          ),
+        divider,
+        Container(
+          height: 400,
+        ),
+        Container(
+          color: Colors.black38,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.people),
+                      )
+                    ],
+                  ),
+
+                Column(
+                  children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.person),
+                      ),                    ],
+                  ),
+
+                Column(
+                  children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.payment),
+                      ),                    ],
+                  ),
+
+              ],
+            ),
+          ),
         ],
       ),
     );
