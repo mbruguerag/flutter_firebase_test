b740581c62a2be334d6bf30346803a447c938dea
Thalia Diaz Juarez <thaliadiaz@MBP-de-Thalia.home>
2020-01-12 16:04:44 +0100

    Disseny sencer de l'app

---
+++ lib/pages/RequestContact.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:firebase/pages/appbars/appbarrequestcontact.dart';
+import 'package:firebase/pages/bodies/bodyrequestcontact.dart';
+class RequestContact extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: headerrequestcontact(),
+      body: BodyRequestContacts(),
+    );
+  }
+}
--- lib/pages/appbars/appbarlogin.dart
+++ lib/pages/appbars/appbarlogin.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 
-AppBar headeraddcontacts() {
+AppBar headerlogin() {
   return AppBar(
     backgroundColor: Colors.white,
           title: Padding(
-            padding: const EdgeInsets.only(left:59),
-            child: Image.asset('assets/monefylogo.jpg', scale: 1.5,),
+            padding: const EdgeInsets.only(left:20),
+            child: Image.asset('assets/monefylogo.jpg', scale:1.5),
           ),
          elevation: 0.0,
   );
