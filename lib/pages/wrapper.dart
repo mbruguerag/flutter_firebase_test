21b3c1dcb45a0d608ca6920e3e7200550f53d9a9
Marc <mbruguerag@gmail.com>
2020-01-13 20:33:43 +0100

    Revert "funcionament no acabat login, register..."
    This reverts commit c5404e72820be6dbd81c1c3efe09ba939533011f.

--- lib/pages/wrapper.dart
+++ lib/pages/wrapper.dart
@@ -1,11 +1,7 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:firebase/models/user.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase/pages/Inicio.dart';
-import 'package:firebase/authenticates/authenticates.dart';
-import 'package:firebase/authenticates/LogIn.dart';
 
 class Wrapper extends StatelessWidget {
   @override
@@ -13,9 +9,9 @@ class Wrapper extends StatelessWidget {
     final user = Provider.of<User>(context);
 
     if (user == null) {
-      return LogIn();
+      return Container(child: Text('Authenticate'));
     } else {
-      return Authentificate();
+      return Inicio();
     }
   }
 }
